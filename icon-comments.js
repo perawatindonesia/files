853c46005f8b15ec3111c97e864ee6c878feab9b
Perawat <infoperawatindonesia7@gmail.com>
2017-04-29 09:26:32 +0700

    Create icon-comments.js

---
+++ icon-comments.js
@@ -0,0 +1,5 @@
+(function() {
+var v = document.createElement('script'); v.async = true;
+v.src = "http://assets-prod.vicomi.com/vicomi.js";
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(v, s);
+})();
